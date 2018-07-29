96c75cfbfb686d95659b15e3f8e1680737c059e5
Porag <mofidulislamporag@gmail.com>
2018-07-29 06:41:29 +0600

    emiting method

--- index.js
+++ index.js
@@ -13,5 +13,9 @@ app.use(express.static('public'));
 var io = socket(server);
 
 io.on('connection',function (socket) {
-	console.log('made socket connection',socket.id)
+	console.log('made socket connection',socket.id);
+
+	socket.on('chat',function (data) {
+		io.sockets.emit('chat',data);
+	});
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,22 @@
 //Make Connection
 var socket = io.connect('http://localhost:4000/');
+
+//Query Dom
+var message = document.getElementById('message');
+handle = document.getElementById('handle'),
+btn = document.getElementById('send'),
+output = document.getElementById('output');
+
+//Emit events
+btn.addEventListener('click',function(){
+	socket.emit('chat',{
+		message:message.value,
+		handle:handle.value 
+	});
+});
+
+//Listen for events
+socket.on('chat',function(data){
+	output.innerHTML +='<p><strong>'+data.handle +':</strong>' +
+data.message + '</p>';
+});
